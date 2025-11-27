3f5ca876706669e2740b82519786f58b346740e3
FireIsGood <fireisadequate@gmail.com>
2025-11-27 13:15:52 -0800

    fix: wait the action does this already

--- svelte.config.js
+++ svelte.config.js
@@ -15,8 +15,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'rns-seedy'
-			// hard coded lmao
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
